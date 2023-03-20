572c1d90c9f18eac94677020353773fdbca36a90
tranbinhminh1403 <diobrando1403@gmail.com>
2023-03-20 21:15:57 +0700

    ver_0

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c644e71e2fmsh23865d295425abap1754dajsnabbd6546cc2a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9f7256db3bd0e94d8f07a7b3252257fd";
